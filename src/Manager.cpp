7380073b20fb66ed0bdb34e4ba7c74f2033b968d
Bruno Pessanha <pessanha@rhrk.uni-kl.de>
2018-11-28 11:58:21 +0100

    TEST ManagerTableShouldHaveTwoNodes - SUCCESS

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -6,6 +6,18 @@ using namespace ClassProject;
 
 Manager::Manager(std::string name) : name(name) {
     bdd_count = 0;
+
+    BDD_Node_t node_0;
+    node_0.label = "0";
+    node_0.bdd = bdd_count;
+    uniqueTable.insert(std::make_pair(node_0.bdd,node_0));
+    bdd_count++;
+
+    BDD_Node_t node_1;
+    node_1.label = "1";;
+    node_1.bdd = bdd_count;
+    uniqueTable.insert(std::make_pair(node_1.bdd,node_1));
+    bdd_count++;
 }
 
 BDD_ID Manager::createVar(const std::string &label) {
@@ -13,6 +25,6 @@ BDD_ID Manager::createVar(const std::string &label) {
 }
 
 size_t Manager::uniqueTableSize() {
-    return 0;
+    return uniqueTable.size();
 }
 

1c52563f9d7698185e108d9a08dffc3bde952691
Bruno Pessanha <pessanha@rhrk.uni-kl.de>
2019-01-30 18:45:36 +0100

    Changes after the review

--- src/reachable/Reachable.cpp
+++ src/reachable/Reachable.cpp
@@ -12,7 +12,7 @@ Reachable::Reachable(unsigned int stateSize) : ReachableInterface(stateSize) {
     this->states.reserve(stateSize*2);
     for (int i = 0; i < stateSize; i++) {
         this->states.push_back(createVar("S" + std::to_string(i)));
-    } //todo: add the conjunction
+    }
     for (int i = 0; i < stateSize; i++) {
         this->states.push_back(createVar("S'" + std::to_string(i)));
     }
@@ -35,14 +35,15 @@ void Reachable::setDelta(const std::vector<BDD_ID> &transitionFunctions) {
 }
 
 void Reachable::setInitState(const std::vector<bool>& stateVector) {
-    for (int i = 0; i < stateVector.size() - 1; i++) {
-        and2(stateVector[i] ? this->states[i] : neg(this->states[i]), stateVector[i+1] ? this->states[i+1] : neg(this->states[i+1]));
+    BDD_ID state = BDD_ID_1;
+    for (int i = 0; i < stateSize; i++) {
+        state = and2(state, stateVector[i] ? this->states[i] : neg(this->states[i]));
     }
 }
 
 BDD_ID Reachable::computeTransitionRelations() {
-    BDD_ID relations_tau = getTransitionRelation(0);
-    for (int i = 1; i < stateSize; i++) {
+    BDD_ID relations_tau = BDD_ID_1;
+    for (int i = 0; i < stateSize; i++) {
         relations_tau = and2(relations_tau, getTransitionRelation(i));
     }
     //std::cout << "TRANSITION RELATIONS" << std::endl;
@@ -51,44 +52,47 @@ BDD_ID Reachable::computeTransitionRelations() {
 }
 
 BDD_ID Reachable::compute_reachable_states() {
-    return compute_reachable_state(0);
-}
-
-BDD_ID Reachable::compute_reachable_state(int state) {
     BDD_ID char_R = False();
     if (stateSize > 0) {
         BDD_ID relations_tau = computeTransitionRelations();
-        BDD_ID char_R_it = getCharacteristicFunction(state);
+        BDD_ID char_R_it = getCharacteristicFunction();
         int index = 0;
         do {
             char_R = char_R_it;
-            BDD_ID image = computeImage(index++, relations_tau, char_R);
+            BDD_ID image = computeImage(relations_tau, char_R); //calculate for all states
             char_R_it = or2(char_R, image);
-        } while (index < stateSize - 1);
+        } while (char_R != char_R_it);
     }
     return char_R;
 }
 
-BDD_ID Reachable::computeImage(int index, BDD_ID relations_tau, BDD_ID char_R) {
-    /*Compute img(s0',s1')*/
-    BDD_ID temp1 = and2(char_R, relations_tau);
-    BDD_ID temp2 = or2(coFactorTrue(temp1, states[index+1]), coFactorFalse(temp1, states[index+1]));
-    BDD_ID img_S0S1_next = or2(coFactorTrue(temp2, states[index]), coFactorFalse(temp2, states[index]));
+BDD_ID Reachable::computeImage(BDD_ID relations_tau, BDD_ID char_R) {
+    BDD_ID img_SOS1;
 
-    BDD_ID s0_prime = states[stateSize + index];
-    BDD_ID s1_prime = states[stateSize + index + 1];
+    for (int i = 0; i < stateSize - 1; i++) {
+        BDD_ID temp1 = and2(char_R, relations_tau);
+        BDD_ID temp2 = or2(coFactorTrue(temp1, states[i+1]), coFactorFalse(temp1, states[i+1]));
+        BDD_ID img_S0S1_next = or2(coFactorTrue(temp2, states[i]), coFactorFalse(temp2, states[i]));
 
-    /*Compute img(s0,s1)*/
-    temp1 = and2(xnor2(states[index],s0_prime), xnor2(states[index+1],s1_prime));
-    temp2 = and2(temp1, img_S0S1_next);
-    BDD_ID temp3 = or2(coFactorTrue(temp2, s1_prime), coFactorFalse(temp2, s1_prime));
-    BDD_ID img_SOS1 = or2(coFactorTrue(temp3, s0_prime), coFactorFalse(temp3, s0_prime));
+        BDD_ID s0_prime = states[stateSize + i];
+        BDD_ID s1_prime = states[stateSize + i + 1];
+
+        /*Compute img(s0,s1)*/
+        temp1 = and2(xnor2(states[i],s0_prime), xnor2(states[i+1],s1_prime));
+        temp2 = and2(temp1, img_S0S1_next);
+        BDD_ID temp3 = or2(coFactorTrue(temp2, s1_prime), coFactorFalse(temp2, s1_prime));
+        img_SOS1 = or2(coFactorTrue(temp3, s0_prime), coFactorFalse(temp3, s0_prime));
+    }
 
     return img_SOS1;
 }
 
-BDD_ID Reachable::getCharacteristicFunction(int index) {
-    return and2(xnor2(states[index],0),xnor2(states[index+1], 0));
+BDD_ID Reachable::getCharacteristicFunction() { //calculate for initial state
+    BDD_ID characteristc = BDD_ID_1;
+    for (int i = 0; i < stateSize; i++) { //and2 with the previous one
+        characteristc = and2(xnor2(characteristc,0),xnor2(states[i], 0));
+    }
+    return characteristc;
 }
 
 BDD_ID Reachable::getTransitionRelation(int index) {
@@ -97,27 +101,10 @@ BDD_ID Reachable::getTransitionRelation(int index) {
 }
 
 bool Reachable::is_reachable(const std::vector<bool>& stateVector) {
-    BDD_ID representation_of_reachable_states = compute_reachable_states();
-    BDD_ID cofactors[stateSize];
-    BDD_ID universal_quant;
-
-    for (int i = 0; i < stateSize - 1; i++) {
-        if(stateVector[i])
-            cofactors[i] = coFactorTrue(this->states[i],representation_of_reachable_states);
-        else
-            cofactors[i] = coFactorFalse(this->states[i],representation_of_reachable_states);
-    }
-
-    for (int i = 0; i < stateSize - 1; i++) {
-        if(0 == i) universal_quant = cofactors[i];
-        else{
-            universal_quant = and2(universal_quant,cofactors[i]);
-        }
-    }
-
-    if( universal_quant == representation_of_reachable_states) return true;
-    else false;
-
-
+    BDD_ID reachable = compute_reachable_states();
+    for (int i = 0; i < stateSize; i++) {
+        reachable = stateVector[i] ? coFactorTrue(reachable, this->states[i]) : coFactorFalse(reachable, this->states[i]);
+    } //doCofactor with the previous value
+    return reachable == BDD_ID_1;
 }
 

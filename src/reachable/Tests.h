b886a02a68883785b2611a5834b5bb0ca7247ebe
Hector <hector.fr89@gmail.com>
2019-02-03 23:57:55 +0100

    TEST More_than_2_states - FAIL

--- src/reachable/Tests.h
+++ src/reachable/Tests.h
@@ -68,5 +68,31 @@ TEST(managerTest, exceptionErrorCheck_noDelta) {
     ASSERT_FALSE(comp.is_reachable({true,true}));
 }
 
+TEST(managerTest, More_than_2_states) {
+    ClassProject::Reachable comp(3);
+
+    auto states = comp.getStates();
+    std::vector<BDD_ID> functions;
+
+    auto s0 = states.at(0);
+    auto s1 = states.at(1);
+    auto s2 = states.at(2);
+    //s0' = not(s0)
+    functions.push_back(comp.neg(s0));
+    //s1' = not(s1)
+    functions.push_back(comp.neg(s1));
+    //s2' = not(s2)
+    functions.push_back(comp.neg(s2));
+    //Add transition functions
+    comp.setDelta(functions);
+    //Add init state
+    comp.setInitState({false,false,true});
+
+    ASSERT_TRUE(comp.is_reachable({true,true,false}));
+    ASSERT_TRUE(comp.is_reachable({false,false,false}));
+    ASSERT_FALSE(comp.is_reachable({true,false,false}));
+    ASSERT_FALSE(comp.is_reachable({false,true,true}));
+}
+
 
 #endif //VDSPROJECT_TESTS_H
